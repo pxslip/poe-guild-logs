5fce80b71d3b1508f153f0bddb8ae83d2efbe788
Will Kruse <wskruse@gmail.com>
2020-09-27 15:40:32 -0400

    Fix purgecss issues

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    "vue-cli-plugin-tailwind/purgecss": {},
+    'vue-cli-plugin-tailwind/purgecss': {
+      whitelistPatterns: [/^multiselect/, /^router-link/],
+      safelist: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/,
+      ],
+    },
     autoprefixer: {},
   },
 };
