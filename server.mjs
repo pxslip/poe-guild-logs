e3446538155078b2ef7c8c9e1d47fc64b54a3786
Will Kruse <wskruse@gmail.com>
2020-09-27 15:13:03 -0400

    Simple single file backend express server

---
+++ server.mjs
@@ -0,0 +1,44 @@
+import express from 'express';
+import path from 'path';
+import axios from 'axios';
+import history from 'connect-history-api-fallback';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const api = axios.create({
+  baseURL: 'https://www.pathofexile.com/api',
+});
+
+const app = express();
+
+const endpoints = {
+  async 'guild-logs'(queries) {
+    const params = {};
+    if (queries.from !== '0') {
+      params.from = queries.from;
+    }
+    const response = await api.get(`/guild/${queries.guildId}/stash/history`, {
+      headers: {
+        Cookie: `POESESSID=${queries.sessionId};`,
+      },
+      params: params,
+    });
+    return response.data.entries;
+  },
+};
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/', history());
+
+const port = process.env.PORT || 3000;
+
+app.get('/proxy/:endpoint', async (req, res) => {
+  const data = await endpoints[req.params.endpoint](req.query);
+  return res.json(data);
+});
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV !== 'PROD') {
+    console.log(`listening on ${port}`);
+  }
+});
