419393defd5a297155d61a5fb7d604758b9261d2
Will Kruse <wskruse@gmail.com>
2020-09-27 22:07:25 -0400

    Add a link to the github page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,10 @@
   <div id="nav" class="flex flex-row bg-gray-200 mb-2 py-2 text-black">
     <router-link to="/" class="mx-2">PoE Guild Logs</router-link> |
     <router-link to="/about" class="mx-2">About</router-link>
+    <a href="https://github.com/pxslip/poe-guild-logs" class="ml-auto">
+      <img src="../assets/github.png" class="ml-auto mr-2" />
+      GitHub
+    </a>
   </div>
 </template>
 
