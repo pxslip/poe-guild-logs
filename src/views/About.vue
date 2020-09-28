a15b61174b1cf928119c62096eee87781f675704
Will Kruse <wskruse@gmail.com>
2020-09-27 22:12:49 -0400

    Actually add an about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>PoE Guild Logs</h1>
+    <p>
+      PoE Guild Logs is an open source tool for Guild Officers who need to quickly review guild logs to identify issues
+      with guild members.
+    </p>
+    <hr class="my-4" />
+    <p>
+      If you run into issues or would like a new feature please
+      <a href="https://github.com/pxslip/poe-guild-logs/issues" class="text-blue-700 hover:text-blue-500">
+        open an issue
+      </a>
+    </p>
   </div>
 </template>
