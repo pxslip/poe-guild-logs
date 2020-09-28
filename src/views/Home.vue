a9f111e14a5cffbf1aa5f371857c857a1e4f9630
Will Kruse <wskruse@gmail.com>
2020-09-27 22:43:26 -0400

    A little more cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -45,9 +45,24 @@
         <button
           type="button"
           class="border rounded border-gray-700 bg-gray-300 text-gray-900 hover:border-gray-900 hover:bg-gray-700 hover:text-gray-200 p-1 mx-2 w-auto lg:mt-6 mt-2"
+          :disabled="loading"
           @click="load"
         >
-          Load Logs
+          <span v-if="!loading">Load Logs</span>
+          <svg
+            v-if="loading"
+            class="animate-spin h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
         </button>
       </div>
     </section>
@@ -129,10 +144,12 @@ export default {
       actionFilters: [],
       searchText: '',
       from: null,
+      loading: false,
     };
   },
   methods: {
     async load() {
+      this.loading = true;
       try {
         const params = {
           guildId: this.guildId,
@@ -152,6 +169,7 @@ export default {
           'There was an error retrieving the guild logs, please check the console for more information.',
         );
       }
+      this.loading = false;
     },
     toDateTimeStringFromEpoch(ts) {
       const date = new Date(ts * 1000);
