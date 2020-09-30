110f4d028b39f030483716a4ea4ee382fa3c8e25
Will Kruse <wskruse@gmail.com>
2020-09-30 01:50:50 -0400

    Try to preserve the border of the multiselects

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     'vue-cli-plugin-tailwind/purgecss': {
-      whitelistPatterns: [/^multiselect/, /^router-link/],
+      whitelistPatterns: [/^multiselect/, /^router-link/, /data-v-.*/],
       safelist: [
         /-(leave|enter|appear)(|-(to|from|active))$/,
         /^(?!(|.*?:)cursor-move).+-move$/,
