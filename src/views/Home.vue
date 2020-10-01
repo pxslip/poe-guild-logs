122e266b7c0f16db2926dec00a2e562672f21834
Will Kruse <wskruse@gmail.com>
2020-09-30 23:48:53 -0400

    Add a csv export, display count

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -90,46 +90,77 @@
             ></vue-multiselect
           ></label>
         </div>
+        <div class="flex flex-col lg:flex-row">
+          <label class="mx-2 flex-1">
+            Start Date/Time
+            <input
+              type="datetime-local"
+              class="block border border-gray-700 rounded p-2 w-full"
+              v-model="startDateTime"
+            />
+          </label>
+          <label class="mx-2 flex-1">
+            End Date/Time
+            <input
+              type="datetime-local"
+              class="block border border-gray-700 rounded p-2 w-full"
+              v-model="endDateTime"
+            />
+          </label>
+        </div>
       </section>
       <hr class="my-4" />
-      <section class="output">
+      <section class="output relative">
         <p v-if="noneFound" class="font-bold italic text-center text-lg">
           No Logs Found
         </p>
-        <ul v-else>
-          <li class="flex flex-col lg:flex-row border-b-2 border-gray-700 py-2">
-            <div class="mr-4 flex-1">Player</div>
-            <div class="mr-4 flex-1">Action</div>
-            <div class="mr-4 flex-1">Item</div>
-            <div class="mr-4 flex-1">League</div>
-            <div class="flex-1">Time</div>
-          </li>
-          <li
-            v-for="(log, index) in filteredLogs"
-            :key="log.id"
-            :class="['flex', 'flex-col', 'lg:flex-row', 'py-2', { 'bg-gray-100': index % 2 === 0 }]"
+        <div class="absolute right-0">
+          <button
+            class="w-5 mr-2 border rounded border-gray-400 bg-gray-200 text-gray-900 hover:border-gray-600 hover:bg-gray-400 hover:text-gray-900"
+            aria-label="Download Filtered Results"
+            @click="exportResults"
           >
-            <div class="mr-4 flex-1">{{ log.account.name }}</div>
-            <div class="mr-4 flex-1">{{ log.action }}</div>
-            <div class="mr-4 flex-1">{{ log.item }}</div>
-            <div class="mr-4 flex-1">{{ log.league }}</div>
-            <div class="flex-1">{{ toDateTimeStringFromEpoch(log.time) }}</div>
-          </li>
-        </ul>
-        <button
-          v-if="hasMore"
-          :disabled="!canLoadMore"
-          class="w-full border rounded border-gray-700 bg-gray-300 text-gray-900 hover:border-gray-900 hover:bg-gray-700 hover:text-gray-200 p-2 lg:mt-6 mt-2"
-          @click="load(false)"
-        >
-          <template v-if="!loading">
-            <template v-if="canLoadMore">
-              Load More Records
+            <Download />
+          </button>
+        </div>
+        <div v-if="!noneFound">
+          <p class="italic text-center">Showing {{ filteredLogs.length }} of {{ logs.length }} actions</p>
+          <ul>
+            <li class="flex flex-col lg:flex-row border-b-2 border-gray-700 py-2">
+              <div class="mr-4 flex-1 pl-2">Player</div>
+              <div class="mr-4 flex-1">Action</div>
+              <div class="mr-4 flex-1">Item</div>
+              <div class="mr-4 flex-1">League</div>
+              <div class="flex-1">Time</div>
+            </li>
+            <li
+              v-for="(log, index) in filteredLogs"
+              :key="log.id"
+              :class="['flex', 'flex-col', 'lg:flex-row', 'py-2', { 'bg-gray-100': index % 2 === 0 }]"
+            >
+              <div class="mr-4 flex-1 pl-2">{{ log.account.name }}</div>
+              <div class="mr-4 flex-1">{{ log.action }}</div>
+              <div class="mr-4 flex-1">{{ log.item }}</div>
+              <div class="mr-4 flex-1">{{ log.league }}</div>
+              <div class="flex-1">{{ toDateTimeStringFromEpoch(log.time) }}</div>
+            </li>
+          </ul>
+
+          <button
+            v-if="hasMore"
+            :disabled="!canLoadMore"
+            class="w-full border rounded border-gray-700 bg-gray-300 text-gray-900 hover:border-gray-900 hover:bg-gray-700 hover:text-gray-200 p-2 lg:mt-6 mt-2"
+            @click="load(false)"
+          >
+            <template v-if="!loading">
+              <template v-if="canLoadMore">
+                Load More Records
+              </template>
+              <template v-else> Currently Rate Limited, Please Wait {{ this.waitTimeLeft }}s </template>
             </template>
-            <template v-else> Currently Rate Limited, Please Wait {{ this.waitTimeLeft }}s </template>
-          </template>
-          <Spinner v-else class="animate-spin h-5 w-5 mx-auto"></Spinner>
-        </button>
+            <Spinner v-else class="animate-spin h-5 w-5 mx-auto"></Spinner>
+          </button>
+        </div>
       </section>
     </div>
   </DropOverlay>
@@ -137,13 +168,16 @@
 
 <script>
 import axios from 'axios';
+import stringify from 'csv-stringify/lib/sync';
+import { saveAs } from 'file-saver';
 import VueMultiselect from 'vue-multiselect';
 import Spinner from '@/assets/Spinner.svg';
 import QuestionMarkCircle from 'heroicons/outline/question-mark-circle.svg';
+import Download from 'heroicons/outline/download.svg';
 import DropOverlay from '@/components/DropOverlay';
 export default {
   name: 'Home',
-  components: { VueMultiselect, Spinner, QuestionMarkCircle, DropOverlay },
+  components: { VueMultiselect, Spinner, QuestionMarkCircle, DropOverlay, Download },
   data() {
     return {
       sessionId: '',
@@ -153,6 +187,8 @@ export default {
       itemFilters: [],
       actionFilters: [],
       leagueFilters: [],
+      startDateTime: null,
+      endDateTime: null,
       searchText: '',
       daysBack: 5,
       loading: false,
@@ -231,6 +267,49 @@ export default {
       this.waitUntil = null;
       this.logs = json.entries;
     },
+    exportResults() {
+      const results = this.filteredLogs.map(row => {
+        return {
+          id: row.id,
+          item: row.item,
+          account: row.account.name,
+          action: row.action,
+          league: row.league,
+          time: this.toDateTimeStringFromEpoch(row.time),
+        };
+      });
+      const csv = stringify(results, {
+        columns: [
+          {
+            key: 'account',
+            header: 'Account Name',
+          },
+          {
+            key: 'action',
+            header: 'Action',
+          },
+          {
+            key: 'id',
+            header: 'ID',
+          },
+          {
+            key: 'item',
+            header: 'Item',
+          },
+          {
+            key: 'league',
+            header: 'League',
+          },
+          {
+            key: 'time',
+            header: 'Date/Time',
+          },
+        ],
+        header: true,
+      });
+      const blob = new Blob([csv], { type: 'text/csv;charset=utf-8' });
+      saveAs(blob, `poe-guild-logs-${Date.now()}.csv`);
+    },
   },
   computed: {
     uniquePlayers() {
@@ -281,6 +360,14 @@ export default {
         }
         if (this.leagueFilters.length > 0) {
           keep = keep && this.leagueFilters.includes(logObj.league);
+        }
+        if (this.startDateTime) {
+          const dt = new Date(this.startDateTime);
+          keep = keep && dt.getTime() < parseInt(logObj.time) * 1000;
+        }
+        if (this.endDateTime) {
+          const dt = new Date(this.endDateTime);
+          keep = keep && dt.getTime() > parseInt(logObj.time) * 1000;
         }
         return keep;
       });
